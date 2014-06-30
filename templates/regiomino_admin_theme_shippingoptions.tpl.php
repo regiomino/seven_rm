490a6b68c34b75092fd3d90568503cbdc5ac4ae5
root <root@ubuntu.(none)>
2014-06-30 17:58:08 +0200

    Optimiertes Template zur Darstellung von Lieferoptionen

--- templates/regiomino_admin_theme_shippingoptions.tpl.php
+++ templates/regiomino_admin_theme_shippingoptions.tpl.php
@@ -1,6 +1,10 @@
 <?php echo $vars['output'] . $vars['pager']; ?>
 
-<div class="map-wrapper">
-	<h2>Karte der Lieferstrecke <?php echo $vars['fci']->field_metro_name[LANGUAGE_NONE][0]['value']; ?></h2>
-	<div id="map" style="height: 500px"></div>
-</div>
+<?php if(isset($vars['fci']->field_metro_name[LANGUAGE_NONE][0]['value'])): ?>
+	<div class="map-wrapper">
+		<h2>Karte der Lieferstrecke <?php echo $vars['fci']->field_metro_name[LANGUAGE_NONE][0]['value']; ?></h2>
+		<p>ROT: Abhol-PLZ<br />
+		GRÜN: Liefer-PLZ</p>
+		<div id="map" style="height: 500px"></div>
+	</div>
+<?php endif; ?>
